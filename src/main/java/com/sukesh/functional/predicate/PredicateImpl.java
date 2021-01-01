06bc91ccb35989239a94ccf79e203125501a5a1e
sukeshrs <sukesrs@gmail.com>
2021-01-01 13:21:24 -0800

    Predicate usage

---
+++ src/main/java/com/sukesh/functional/predicate/PredicateImpl.java
@@ -0,0 +1,27 @@
+package com.sukesh.functional.predicate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.function.Predicate;
+
+public class PredicateUsage {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(PredicateUsage.class);
+
+
+    public static void main(String[] args) {
+        //Java 7 way
+        Predicate<String> p = new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                return  s.contains("and");
+            }
+        };
+
+        //Java 8 way
+        Predicate<String> pred = s -> s.contains("and");
+        LOGGER.info("Contains 'and' ? : " + pred.test("Jack and Jill"));
+        LOGGER.info("Contains 'and' ? : " + pred.test("Jack or Jill"));
+    }
+}
