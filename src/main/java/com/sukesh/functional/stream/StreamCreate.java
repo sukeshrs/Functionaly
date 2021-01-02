772b7e35dc6a5d340e7342b2a9ee9cd3da480719
sukeshrs <sukesrs@gmail.com>
2021-01-02 14:41:49 -0800

    Stream creation

---
+++ src/main/java/com/sukesh/functional/stream/StreamCreate.java
@@ -0,0 +1,27 @@
+package com.sukesh.functional.stream;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamCreate {
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.empty();
+
+        stream =  Stream.of("uno");
+
+        stream = Stream.of("one", "two", "three");
+
+        // A constant stream
+        stream = Stream.generate(() -> "one");
+
+        stream = Stream.iterate("+", s -> s + "+");
+
+        IntStream intStream = ThreadLocalRandom.current().ints();
+
+        intStream.forEach(System.out::println);
+
+        IntStream characters = "hello".chars();
+        characters.forEach(System.out::println);
+    }
+}
