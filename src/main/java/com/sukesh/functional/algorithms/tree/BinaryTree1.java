1d6a7850982ade4bcd24989186a83cbf49a543cb
sukeshrs <sukesrs@gmail.com>
2022-07-06 15:53:20 -0700

    Added BFS traversals

--- src/main/java/com/sukesh/functional/algorithms/tree/BinaryTree1.java
+++ src/main/java/com/sukesh/functional/algorithms/tree/BinaryTree1.java
@@ -1,6 +1,7 @@
 package com.sukesh.functional.algorithms.tree;
 
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Binary Tree which uses the Node1 class for storing the node .
@@ -34,5 +35,101 @@ public class BinaryTree1 {
             depth= Math.max(maxDepth(node.left, i), maxDepth(node.right,i));
         }
         return depth;
+    }
+
+
+    public List<List> leveOrder(Node1 root) {
+        List<List> levelOrderList = new ArrayList<>();
+        //Add the root node to a queue
+        Queue<Node1> nodes = new LinkedList<>();
+        nodes.add(root);
+        AtomicInteger depth = new AtomicInteger(0);
+        traverseTheTree(nodes , levelOrderList , depth);
+        System.out.println(depth);
+        return levelOrderList;
+    }
+
+
+    /**
+     *          1
+     *       2    6
+     *     3   7
+     * Gets the right side view of the tree
+     * The right side view of the above tree is [1,6,7]
+     * @param root
+     * @return : a list with the right side view os the tree
+     */
+    public List<Integer> getTheRighSideViewOfTheTree(Node1 root) {
+        Queue<Node1> entries = new LinkedList<>();
+        entries.add(root);
+        List<Integer> rightEntries = new ArrayList<>();
+        traverseBreadthWise(entries , rightEntries);
+        return rightEntries;
+    }
+
+
+    public List<Integer> getRightMostEntriesImproved(Node1 root){
+        List<Integer> list = new ArrayList<>();
+        getRightMostValues(root, list);
+        return list;
+    }
+
+    private int getRightMostValues(Node1 root, List<Integer> list) {
+        Node1 current = root;
+        int rightMostDepth = 0;
+        while(Objects.nonNull(current)){
+            list.add(current.value);
+            current = current.right;
+            ++rightMostDepth;
+        }
+        return rightMostDepth;
+    }
+
+    /**
+     * BFS traversal updates
+     * @param nodes
+     * @param rightEntries
+     */
+    private void traverseBreadthWise(Queue<Node1> nodes, List<Integer> rightEntries) {
+        while(!nodes.isEmpty()){
+            int depth = 0, count = 0 , size = nodes.size();
+            Node1 node=null;
+            while(size > count){
+                node = nodes.remove();
+                if(Objects.nonNull(node.left)){
+                    nodes.add(node.left);
+                }
+                if(Objects.nonNull(node.right)){
+                    nodes.add(node.right);
+                }
+                count++;
+            }
+            rightEntries.add(node.value);
+        }
+
+    }
+
+
+    private void traverseTheTree(Queue<Node1> nodes, List<List> levelOrderList, AtomicInteger depth) {
+
+        while(!nodes.isEmpty()){
+            int size = nodes.size();
+            int count = 0;
+            List<Integer> entries = new ArrayList<>();
+            while(size> count){
+                Node1 node = nodes.remove();
+                if(Objects.nonNull(node.left)){
+                    nodes.add(node.left);
+                }
+                if(Objects.nonNull(node.right)){
+                    nodes.add(node.right);
+                }
+                count++;
+                entries.add(node.value);
+            }
+            levelOrderList.add(entries);
+            depth.getAndIncrement();
+        }
+
     }
 }
--- src/test/java/com/sukesh/functional/algorithms/tree/BinaryTree1Test.java
+++ src/test/java/com/sukesh/functional/algorithms/tree/BinaryTree1Test.java
@@ -2,6 +2,9 @@ package com.sukesh.functional.algorithms.tree;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinaryTree1Test {
@@ -43,16 +46,66 @@ public class BinaryTree1Test {
     @Test
     public void itTestsTheMaxDepthOfBinaryTree_WhenTheTreeIsBalanced(){
         classToTest = new BinaryTree1();
+        Node1 node1 = createNode();
+        int depth = classToTest.findMaxDepth(node1);
+        assertEquals(3, depth);
+    }
+
+
+    @Test
+    public void itReturnsTheLevelOrderOfTheTree(){
+        classToTest = new BinaryTree1();
+        Node1 root = createNode();
+        List firstLevel = Arrays.asList(1);
+        List<List> levelOrderList = classToTest.leveOrder(root);
+        assertEquals(levelOrderList.get(0), firstLevel);
+        System.out.println(levelOrderList.get(2));
+    }
+
+    @Test
+    public void itTestsTheListOfRightMostEntries(){
+        classToTest = new BinaryTree1();
+        Node1 root = createUnbalancedNode();
+        List<Integer> expected = Arrays.asList(1,0,0,0);
+        List<Integer> rights = classToTest.getTheRighSideViewOfTheTree(root);
+        assertEquals(expected, rights);
+    }
+
+    @Test
+    public void itTestsTheListOfRightMostEntriesUsingAnotherMethod(){
+        classToTest = new BinaryTree1();
+        Node1 root = createUnbalancedNode();
+        List<Integer> expected = Arrays.asList(1,0,0);
+        List<Integer> rights = classToTest.getRightMostEntriesImproved(root);
+        assertEquals(expected, rights);
+    }
+
+    private Node1 createNode() {
         Node1 node1 = new Node1();
         Node1 node2 = new Node1();
         Node1 node3 = new Node1();
         Node1 node4 = new Node1();
         Node1 node5 = new Node1();
+        node1.value = 1;
         node1.left = node2;
         node1.right = node3;
         node2.left = node4;
         node3.right = node5;
-        int depth = classToTest.findMaxDepth(node1);
-        assertEquals(3, depth);
+        return node1;
+    }
+
+    private Node1 createUnbalancedNode() {
+        Node1 node1 = new Node1();
+        Node1 node2 = new Node1();
+        Node1 node3 = new Node1();
+        Node1 node4 = new Node1();
+        Node1 node5 = new Node1();
+        node1.value = 1;
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node4.left = new Node1();
+        node3.right = node5;
+        return node1;
     }
 }
