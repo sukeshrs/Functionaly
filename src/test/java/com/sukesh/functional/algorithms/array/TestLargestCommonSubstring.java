ffb78df2916a733a259dabf7f320e7c07e11fd24
sukeshrs <sukesrs@gmail.com>
2023-07-04 14:47:35 -0700

    Refactored the names of the tests

--- src/test/java/com/sukesh/functional/algorithms/array/TestLargestCommonSubstring.java
+++ src/test/java/com/sukesh/functional/algorithms/array/TestLargestCommonSubstring.java
@@ -7,31 +7,38 @@ public class TestLargestCommonSubstring {
     private LargestCommonSubstring uut = new LargestCommonSubstring();
 
     @Test
-    public void itTestsTheLargestCommonSubstring(){
+    public void itTestsTheLargestCommonPrefix(){
         String[] list = {"a", "b", "c"};
         String actual = uut.findLargestCommonPrefix(list);
         Assert.assertEquals("", actual);
     }
 
     @Test
-    public void itTestsTheLargestCommonSubstringMatch(){
+    public void itTestsTheLargestCommonPrefixMatch(){
         String[] list = {"aac", "aba", "aca"};
         String actual = uut.findLargestCommonPrefix(list);
         Assert.assertEquals("a", actual);
     }
 
     @Test
-    public void itTestsTheLargestCommonSubstringNoMatch(){
+    public void itTestsTheLargestCommonPrefixNoMatch(){
         String[] list = {"google", "is", "a" ,"data", "company"};
         String actual = uut.findLargestCommonPrefix(list);
         Assert.assertEquals("", actual);
     }
 
     @Test
-    public void itTestsTheLargestCommonSubstringMatchTwoChars(){
+    public void itTestsTheLargestCommonPrefixMatchTwoChars(){
         String[] list = {"google", "gogle", "golf"};
         String actual = uut.findLargestCommonPrefix(list);
         Assert.assertEquals("go", actual);
+    }
+
+    @Test
+    public void itTestsTheLargestCommonPrefixDoesNotMatch(){
+        String[] list = {"dog", "fog", "jog"};
+        String actual = uut.findLargestCommonPrefix(list);
+        Assert.assertNotEquals("og", actual);
     }
 }
 
